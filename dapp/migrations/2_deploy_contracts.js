c625142cbaff687cb40617afd719b9cf3be8749e
MROS <yc1043@gmail.com>
2018-06-10 23:21:04 +0800

    link Kotodamas with Mating

--- dapp/migrations/2_deploy_contracts.js
+++ dapp/migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const Kotodamas = artifacts.require("./Kotodamas");
+const Mating = artifacts.require("./Mating");
 
 module.exports = function(deployer) {
+  deployer.deploy(Mating);
+  deployer.link(Mating, Kotodamas);
   deployer.deploy(Kotodamas);
 };
